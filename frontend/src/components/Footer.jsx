f3edce34766e1429e975842b0ed2c3c797f00ab6
Michalabiodun <127579173+Michalabiodun@users.noreply.github.com>
2025-09-12 10:34:13 -0700

    Delete frontend directory

--- frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <footer className="footer">
-      © 2025 Kingdom Mandate | All rights reserved.
-    </footer>
-  );
-}
